ee7559a30c694101bb22684f7b7ca3763ad6871b
markoricardo <marko.ricardo@gmail.com>
2025-05-09 15:25:39 -0500

    initial commit

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IProduct } from '../catalog/product.model';
+
+@Component({
+  selector: 'bot-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent {
+  @Input() product!: IProduct;
+  @Output() buy = new EventEmitter();
+  
+
+  
+  buyButtonClicked(product: IProduct) {
+    this.buy.emit();
+ }
+
+  getImageUrl(product: IProduct) {
+    if (!product) return '';
+    return '/assets/images/robot-parts/' + product.imageName;
+  }
+
+
+  getDiscountedClasses(product: IProduct) {
+    if (product.discount > 0) return 'strikethrough';
+    else return '';
+
+  }
+
+
+}
