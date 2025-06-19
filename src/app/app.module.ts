7092e3e2e2e663279f27b5df06f1206daab81a19
markoricardo <marko.ricardo@gmail.com>
2025-06-18 19:58:57 -0500

    sign-in form completed. with validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -9,6 +10,11 @@ import { SiteHeaderComponent} from './site-header/site-header.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -17,9 +23,10 @@ import { AppRoutingModule } from './app-routing.module';
     CatalogComponent,
     SiteHeaderComponent,
     ProductDetailsComponent,
-    CartComponent
+    CartComponent,
+    SignInComponent,
   ],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -2,6 +2,8 @@ import { Component, inject, ViewEncapsulation } from '@angular/core';
 import { IProduct } from './product.model';
 import { ProductService } from './product.service';
 import { CartService } from '../cart/cart.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 
 @Component({
@@ -15,22 +17,30 @@ export class CatalogComponent {
 
   constructor(
     private cartSvc: CartService, 
-    private productSvc: ProductService
+    private productSvc: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit() {
     this.productSvc.getProducts().subscribe((products) => {
       this.products = products;
     });
+    this.route.queryParams.subscribe((params => {
+      this.filter = params['filter'] ?? '';
+    }))
   }
 
   addToCart(product: IProduct) {
     this.cartSvc.add(product);
+    this.router.navigate(['/cart']);
  }
 
   getFilteredProducts() {
     return this.filter === ''
             ? this.products 
-            : this.products.filter((product : any) => product.category === this.filter);
+            : this.products.filter(
+              (product : any) => product.category === this.filter);
   }
 }
+
